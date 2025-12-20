18becb5b4e26cbdff89266a1429004f1f0e6a05c
Guilherme <guilherme.luccas@outlook.com>
2025-12-20 12:06:30 -0300

    feat: 🎸 1493-plans screen
    ✅ Closes: 1493

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,7 +1,5 @@
-import { UserValidationFlow } from "./components/UserValidationFlow/UserValidationFlow";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { PlanCard } from "@/src/components/planCard";
+import { PlanCard } from "@/components/planCard";
+import { PlanCarousel } from "@/components/planCarousel";
 
 type SearchParams = {
   cupom: string;
@@ -18,16 +16,59 @@ async function PlansPage({ searchParams }: PlansPageProps) {
   const planId = params?.planId;
   const couponId = params?.cupom;
 
+  const plans = [
+    {
+      id: "1",
+      price: 85,
+      appointments: 1,
+      isRecommended: false,
+      title: "Plano Essencial",
+      description: "Para quem está começando.",
+      benefits: ["Acesso ilimitado", "Profissionais certificados"],
+    },
+    {
+      id: "2",
+      price: 160,
+      appointments: 2,
+      isRecommended: true,
+      title: "Plano Equilíbrio",
+      description: "O mais escolhido para constância.",
+      benefits: ["Acesso ilimitado", "Profissionais certificados"],
+    },
+    {
+      id: "3",
+      price: 280,
+      appointments: 4,
+      isRecommended: false,
+      title: "Plano Intensivo",
+      description: "Para foco total no processo.",
+      benefits: ["Acesso ilimitado", "Atendimento personalizado"],
+    },
+  ];
+
   return (
-    <div className="min-h-screen flex flex-col sm:flex-row items-center justify-center gap-4 p-4">
-      <UserValidationFlow planId={planId} couponId={couponId} />
-      <Button variant="secondary">Subscribe Now</Button>
-      <Button>Subscribe Now</Button>
-      <div>
-        <Input placeholder="Digite seu email" />
+    <div className="min-h-screen flex flex-col items-center bg-gray-50 pt-10 gap-6 md:gap-8">
+      <div className="px-4 flex flex-col items-center gap-6">
+        <div className="bg-primary-600/10 px-8 py-2 rounded-full">
+          <p className="text-primary-600 text-sm font-bold">
+            ✨ Mais de 40.000 pessoas transformadas
+          </p>
+        </div>
+        <div className="text-center flex flex-col gap-6">
+          <h1 className="text-4-5xl md:text-6-5xl font-black text-blue-950 leading-none md:leading-6-5xl ">
+            Escolha o plano{" "}
+            <span className="text-primary-600">
+              ideal para <br /> sua jornada
+            </span>
+          </h1>
+          <p className="text-gray-600 text-lg font-semibold leading-tight">
+            Escolha o plano ideal e comece sua jornada de autoconhecimento hoje
+            mesmo.
+          </p>
+        </div>
       </div>
 
-      <PlanCard />
+      <PlanCarousel plans={plans} />
     </div>
   );
 }
--- src/components/planCard.tsx
+++ src/components/planCard.tsx
@@ -1,11 +1,96 @@
-export function PlanCard() {
+import { formatPrice } from "@/utils/format-currency";
+import { CheckCheck } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { Button } from "./ui/button";
+
+type PlanCardProps = {
+  id: string;
+  price: number;
+  appointments: number;
+  isRecommended: boolean;
+  title: string;
+  description: string;
+  benefits: string[];
+};
+
+export function PlanCard({
+  id,
+  price,
+  appointments,
+  isRecommended,
+  title,
+  description,
+  benefits,
+}: PlanCardProps) {
+  const sessionLabel = appointments === 1 ? "Sessão" : "Sessões";
+
   return (
-    <div>
-      <h3>Free Plan</h3>
-      <p className="text-gray-600">
-        Get started with our free plan. Limited features available.
-      </p>
-      <p>$0</p>
-    </div>
+    <>
+      {isRecommended && (
+        <div className="bg-primary-600 py-2 text-center rounded-t-2xl">
+          <span className="text-gray-50 text-base font-bold">
+            O Mais Popular
+          </span>
+        </div>
+      )}
+      <div
+        className={cn(
+          "bg-white rounded-2xl overflow-hidden flex flex-col h-full",
+          {
+            "border-2 border-primary-600": isRecommended,
+            "rounded-t-none": isRecommended,
+          }
+        )}
+      >
+        <div className="px-4 pt-4 pb-10 flex flex-col flex-1">
+          <h3 className="text-2-5xl font-bold text-blue-950">{title}</h3>
+          <p className="text-base text-gray-600 pb-6">{description}</p>
+          <div className="flex items-baseline gap-1 pb-1">
+            <span
+              className={cn("text-5xl font-black", {
+                "text-primary-600": isRecommended,
+                "text-blue-950": !isRecommended,
+              })}
+            >
+              {formatPrice(price)}
+            </span>
+            <span className="text-base text-gray-600 font-semibold">/mês</span>
+          </div>
+          <p className="text-sm text-gray-400 font-semibold pb-6">
+            Cobrado mensalmente
+          </p>
+          <div className="flex items-center gap-2 mb-2">
+            <CheckCheck className="text-primary w-5 h-5 shrink-0" />
+            <p className="text-sm text-gray-600 font-semibold">
+              <span
+                className={cn("font-bold", {
+                  "text-primary": isRecommended,
+                  "text-gray-600": !isRecommended,
+                })}
+              >
+                {appointments} {sessionLabel}
+              </span>{" "}
+              de terapia por mês
+            </p>
+          </div>
+          <div className="space-y-2 pb-6">
+            {benefits.map((benefit, index) => (
+              <div key={index} className="flex items-center gap-2 ">
+                <CheckCheck className="text-primary w-5 h-5 shrink-0" />
+                <p className="text-sm text-gray-600 font-semibold">{benefit}</p>
+              </div>
+            ))}
+          </div>
+          <div className="flex justify-center mt-auto">
+            <Button
+              variant={isRecommended ? "primary" : "secondary"}
+              className={cn({ "shadow-cta-glow": isRecommended })}
+            >
+              Quero o plano {title}
+            </Button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
